8d64137d3e1c7d5a4f855528b1ec5d7434a959a4
Hanno Schlichting <hanno@hannosch.eu>
2013-03-02 12:03:42 +0100

    post-git cleanup

--- setup.py
+++ setup.py
@@ -1,36 +1,37 @@
 from setuptools import setup, find_packages
-import os.path
 
-version = '1.2dev'
+__version__ = '1.2dev'
 
-setup(name='plone.postpublicationhook',
-      version=version,
-      description="Zope 2 post-publication hook",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      classifiers=[
+setup(
+    name='plone.postpublicationhook',
+    version=__version__,
+    description="Zope post-publication hook.",
+    long_description=(open("README.rst").read() + "\n" +
+                      open("CHANGES.rst").read()),
+    classifiers=[
         "Programming Language :: Python",
         "Environment :: Web Environment",
         "Framework :: Zope2",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: Zope Public License",
-        ],
-      keywords='',
-      author='Wichert Akkerman',
-      author_email='wichert@wiggy.net',
-      url='',
-      license='ZPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'zope.event',
-          'zope.interface',
-          'zope.security',
-      ],
-      extras_require={
-          'Zope2.10': ['ZPublisherEventsBackport'],
-      },
-      )
+    ],
+    keywords='',
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    url='http://pypi.python.org/pypi/plone.postpublicationhook',
+    license='ZPL',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.event',
+        'zope.interface',
+        'zope.security',
+    ],
+    extras_require={
+        'Zope2.10': ['ZPublisherEventsBackport'],
+    },
+)
